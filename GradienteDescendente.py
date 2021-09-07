23a48e5d798e2bf43afef9b878ad9ece047c58f1
Larissa Almada <62493810+Lalalmada@users.noreply.github.com>
2021-09-07 20:06:29 -0300

    Create GradienteDescendente.py
    é um algoritmo de otimização que realiza o ajuste de parâmetros de forma iterativa com o objetivo de encontrar o valor θ₀ e θ₁ que minimiza a função de interesse. Ou seja, a reta que melhor se ajusta aos dados de uma regressão linear, dado que uma regressão linear é:
    y=θ 
    0
    ​
     +θ 
    1
    ​
     x+ε

---
+++ GradienteDescendente.py
@@ -0,0 +1,13 @@
+>>> from sklearn.datasets import make_regression
+>>> from sklearn.ensemble import GradientBoostingRegressor
+>>> from sklearn.model_selection import train_test_split
+>>> X, y = make_regression(random_state=0)
+>>> X_train, X_test, y_train, y_test = train_test_split(
+...     X, y, random_state=0)
+>>> reg = GradientBoostingRegressor(random_state=0)
+>>> reg.fit(X_train, y_train)
+GradientBoostingRegressor(random_state=0)
+>>> reg.predict(X_test[1:2])
+array([-61...])
+>>> reg.score(X_test, y_test)
+0.4...
